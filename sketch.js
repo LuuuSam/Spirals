f03676ed1efe1008b4c65528a550dce7e7fd2d71
LuuuSam <lourdessampol8@gmail.com>
2023-07-05 08:42:47 -0300

    creo el primer commit

---
+++ sketch.js
@@ -0,0 +1,510 @@
+function setup() {
+    createCanvas(512, 512);
+    background(0);
+  }
+  
+  function draw() {
+    console.log("x:" + mouseX + "y:" + mouseY);
+  
+    // primera fila
+    push();
+    strokeWeight(10);
+    translate(50, 0);
+    ellipse(0, 50, 8, 8);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(100, 0);
+    ellipse(0, 50, 8, 8);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(150, 0);
+    ellipse(0, 50, 8, 8);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(200, 0);
+    ellipse(0, 50, 8, 8);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(250, 0);
+    ellipse(0, 50, 8, 8);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(300, 0);
+    ellipse(0, 50, 8, 8);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(350, 0);
+    ellipse(0, 50, 8, 8);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(400, 0);
+    ellipse(0, 50, 8, 8);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(450, 0);
+    ellipse(0, 50, 8, 8);
+    pop();
+  
+    // segunda fila
+    push();
+    strokeWeight(10);
+    translate(50, 0);
+    ellipse(0, 100, 6, 6);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(100, 0);
+    ellipse(0, 100, 6, 6);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(150, 0);
+    ellipse(0, 100, 6, 6);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(200, 0);
+    ellipse(0, 100, 6, 6);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(250, 0);
+    ellipse(0, 100, 6, 6);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(300, 0);
+    ellipse(0, 100, 6, 6);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(350, 0);
+    ellipse(0, 100, 6, 6);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(400, 0);
+    ellipse(0, 100, 6, 6);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(450, 0);
+    ellipse(0, 100, 6, 6);
+    pop();
+  
+    // tercera fila
+    push();
+    strokeWeight(10);
+    translate(50, 0);
+    ellipse(0, 150, 4, 4);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(100, 0);
+    ellipse(0, 150, 4, 4);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(150, 0);
+    ellipse(0, 150, 4, 4);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(200, 0);
+    ellipse(0, 150, 4, 4);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(250, 0);
+    ellipse(0, 150, 4, 4);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(300, 0);
+    ellipse(0, 150, 4, 4);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(350, 0);
+    ellipse(0, 150, 4, 4);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(400, 0);
+    ellipse(0, 150, 4, 4);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(450, 0);
+    ellipse(0, 150, 4, 4);
+    pop();
+  
+    // cuarta fila
+    push();
+    strokeWeight(10);
+    translate(50, 0);
+    ellipse(0, 200, 2, 2);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(100, 0);
+    ellipse(0, 200, 2, 2);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(150, 0);
+    ellipse(0, 200, 2, 2);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(200, 0);
+    ellipse(0, 200, 2, 2);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(250, 0);
+    ellipse(0, 200, 2, 2);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(300, 0);
+    ellipse(0, 200, 2, 2);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(350, 0);
+    ellipse(0, 200, 2, 2);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(400, 0);
+    ellipse(0, 200, 2, 2);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(450, 0);
+    ellipse(0, 200, 2, 2);
+    pop();
+  
+    // quinta fila
+    push();
+    strokeWeight(10);
+    translate(50, 0);
+    ellipse(0, 250, 8, 8);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(100, 0);
+    ellipse(0, 250, 8, 8);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(150, 0);
+    ellipse(0, 250, 8, 8);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(200, 0);
+    ellipse(0, 250, 8, 8);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(250, 0);
+    ellipse(0, 250, 8, 8);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(300, 0);
+    ellipse(0, 250, 8, 8);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(350, 0);
+    ellipse(0, 250, 8, 8);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(400, 0);
+    ellipse(0, 250, 8, 8);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(450, 0);
+    ellipse(0, 250, 8, 8);
+    pop();
+  
+    // sexta fila
+    push();
+    strokeWeight(10);
+    translate(50, 0);
+    ellipse(0, 300, 6, 6);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(100, 0);
+    ellipse(0, 300, 6, 6);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(150, 0);
+    ellipse(0, 300, 6, 6);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(200, 0);
+    ellipse(0, 300, 6, 6);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(250, 0);
+    ellipse(0, 300, 6, 6);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(300, 0);
+    ellipse(0, 300, 6, 6);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(350, 0);
+    ellipse(0, 300, 6, 6);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(400, 0);
+    ellipse(0, 300, 6, 6);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(450, 0);
+    ellipse(0, 300, 6, 6);
+    pop();
+  
+    // septima fila
+    push();
+    strokeWeight(10);
+    translate(50, 0);
+    ellipse(0, 350, 4, 4);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(100, 0);
+    ellipse(0, 350, 4, 4);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(150, 0);
+    ellipse(0, 350, 4, 4);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(200, 0);
+    ellipse(0, 350, 4, 4);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(250, 0);
+    ellipse(0, 350, 4, 4);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(300, 0);
+    ellipse(0, 350, 4, 4);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(350, 0);
+    ellipse(0, 350, 4, 4);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(400, 0);
+    ellipse(0, 350, 4, 4);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(450, 0);
+    ellipse(0, 350, 4, 4);
+    pop();
+  
+    // octava fila
+    push();
+    strokeWeight(10);
+    translate(50, 0);
+    ellipse(0, 400, 2, 2);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(100, 0);
+    ellipse(0, 400, 2, 2);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(150, 0);
+    ellipse(0, 400, 2, 2);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(200, 0);
+    ellipse(0, 400, 2, 2);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(250, 0);
+    ellipse(0, 400, 2, 2);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(300, 0);
+    ellipse(0, 400, 2, 2);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(350, 0);
+    ellipse(0, 400, 2, 2);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(400, 0);
+    ellipse(0, 400, 2, 2);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(450, 0);
+    ellipse(0, 400, 2, 2);
+    pop();
+  
+    // novena fila
+    push();
+    strokeWeight(10);
+    translate(50, 0);
+    ellipse(0, 450, 0.5, 0.5);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(100, 0);
+    ellipse(0, 450, 0.5, 0.5);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(150, 0);
+    ellipse(0, 450, 0.5, 0.5);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(200, 0);
+    ellipse(0, 450, 0.5, 0.5);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(250, 0);
+    ellipse(0, 450, 0.5, 0.5);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(300, 0);
+    ellipse(0, 450, 0.5, 0.5);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(350, 0);
+    ellipse(0, 450, 0.5, 0.5);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(400, 0);
+    ellipse(0, 450, 0.5, 0.5);
+    pop();
+  
+    push();
+    strokeWeight(10);
+    translate(450, 0);
+    ellipse(0, 450, 0.5, 0.5);
+    pop();
+  
+    ellipse(150, 250, mouseX, mouseY);
+    ellipse(350, 250, mouseX, mouseY);
+    noFill();
+    strokeWeight(0.4);
+    stroke(200);
+  }
+  
